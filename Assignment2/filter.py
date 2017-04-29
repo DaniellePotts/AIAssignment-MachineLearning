562305b8591c7553e588e0b079425c486d5f9fa5
Danielle Potts <danielle-19-93@live.com>
2017-04-29 22:24:42 +0100

    add more filters. add remove empty values and remove non numbers (NaN)

--- Assignment2/filter.py
+++ Assignment2/filter.py
@@ -1,24 +1,35 @@
 import numpy as np
 
 class Filter:
-    data = []
 
-    def __init__(self,data):
-        self.data = data
+    def __init__(self):
+        print('filtering...')
 
-    def AddNoise(self):
+    def AddNoise(self,data):
         noise = np.random.normal(0, 1, 100)
-        lst = list(self.data)
+        lst = list(data)
         for x in range(len(noise)):
-            print(self.data[0])
+            print(data[0])
             lst.append(noise[x])
         return np.array(lst)
 
-    def removeDuplicates(self):
-        return list(set(self.data))
+    def removeDuplicates(self,data):
+        return list(set(data))
 
-    def normalise(self):
-        lst = list(self.data)
+    def normalise(self,data):
+        lst = list(data)
         norm = [float(i)/sum(lst) for i in lst]
         norm = [float(i)/sum(norm) for i in norm]
-        return np.array(norm)
+        result = np.array(norm)
+        return result
+
+    def removeNonNumbers(self,data):
+        return np.array(input[~np.isnan(input)])
+
+    def removeMissingOrEmpty(self,data):
+        lst = list(data)
+        for x in range(len(lst)):
+            if lst[x] == 0.0:
+                lst.remove(lst[x])
+
+        return np.array(lst)
